f1fb339522ab7e22c100a68e1051fb46398301d0
Filipe Moreira <fmc@cin.ufpe.br>
2023-09-12 20:40:34 -0300

    Update main.py

--- CroCIn Road/main.py
+++ CroCIn Road/main.py
@@ -2,6 +2,17 @@ import pygame as pg
 import sys
 import random
 
+import Logica
+import Classes
+
 #Inicializa o funcionamento do PyGame
 pg.init()
 
+#Inicializa o looping de execução e sua variável de controle
+running = True
+while(running):
+    for event in pygame.event.get():
+        #Verifica se houve a solicitação de fim da execução do programa
+        if event.type == pygame.QUIT:
+            running = False
+    

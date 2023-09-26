13ec5d1a07e59167e697bf0f9eeea8732b0156ee
Filipe Moreira <fmc@cin.ufpe.br>
2023-09-26 09:34:53 -0300

    Implementação básica dos coletáveis
    Update coletaveis.py

--- CroCIn Road/coletaveis.py
+++ CroCIn Road/coletaveis.py
@@ -1,68 +1,26 @@
 import pygame
+import os
 
-class Personagem:
+image_dir = os.path.join(os.path.dirname(__file__), 'imagens')
+
+class Coletavel:
         
-        def __init__(self, x, y, tamanho, tela):
+        def __init__(self, x, y, image_filename, tela):
             self.x = x
             self.y = y
-            self.tamanho = tamanho
-            self.cor = (255, 255, 255)
             self.tela = tela
-            self.velocidade_maxima = 2.0
-            self.aceleracao = 0.1
-            self.velocidade_atual = 0.0
-            self.vidas = 3
-            self.coletaveis = 0
+            original_image = pygame.image.load(os.path.join(image_dir, image_filename))
+            self.image = pygame.transform.scale(original_image, (50, 50))  # Redimensionar a imagem
+            self.hitbox = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
 
-            self.is_moving = False
-            self.movement_step = self.tamanho
-            self.hitbox = pygame.Rect(self.x, self.y, tamanho, tamanho)
-
-        def movimento(self, dx, dy):
-            # Atualize a posição do personagem
-            self.x += dx
-            self.y += dy
-
+        def draw(self, surface):
+            surface.blit(self.image, (self.x, self.y))
+            self.hitbox.topleft = (self.x, self.y)  # Atualizar a posição do hitbox
 
         def processar_eventos(self):
-            keys = pygame.key.get_pressed()
-            
             self.hitbox.x = self.x
             self.hitbox.y = self.y
-            
-            # Se o personagem não estiver em movimento e uma tecla for pressionada
-            if not self.is_moving:
-                if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                    self.movimento(-self.movement_step, 0)
-                    self.is_moving = True
-                elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                    self.movimento(self.movement_step, 0)
-                    self.is_moving = True
-                elif keys[pygame.K_UP] or keys[pygame.K_w]:
-                    self.movimento(0, -self.movement_step)
-                    self.is_moving = True
-                elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                    self.movimento(0, self.movement_step)
-                    self.is_moving = True
-
-                
-
-            # Se uma tecla diferente for pressionada, pare o movimento
-            elif (
-                not (keys[pygame.K_LEFT] or keys[pygame.K_a] or
-                    keys[pygame.K_RIGHT] or keys[pygame.K_d] or
-                    keys[pygame.K_UP] or keys[pygame.K_w] or
-                    keys[pygame.K_DOWN] or keys[pygame.K_s])
-            ):
-                self.is_moving = False
-
-            # Verifique os limites da tela
-            if self.x < 0:
-                self.x = 0
-            elif self.x + self.tamanho > self.tela.get_width():
-                self.x = self.tela.get_width() - self.tamanho
-
-            if self.y < 0:
-                self.y = 0
-            elif self.y + self.tamanho > self.tela.get_height():
-                self.y = self.tela.get_height() - self.tamanho
+        
+        def check_colisao(self, player_hitbox):
+            # Verifique a colisão usando o hitbox do jogador
+            return self.hitbox.colliderect(player_hitbox)

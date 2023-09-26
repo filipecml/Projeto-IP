a2313c5a50782f00cc4f451bd2d5df5668c77387
João Guilherme Cavalcanti <joaogui76@gmail.com>
2023-09-26 20:59:23 -0300

    Remoção do rio e correções

--- CroCIn Road/cenario.py
+++ CroCIn Road/cenario.py
@@ -7,11 +7,11 @@ class Cenario:
         
         self.largura = largura
         self.altura = altura
+        self.grama_0 = pygame.image.load(os.path.join(self.current_dir, "imagens", "grama_0.png"))
         self.grama_1 = pygame.image.load(os.path.join(self.current_dir, "imagens", "grama_1.png"))
         self.grama_2 = pygame.image.load(os.path.join(self.current_dir, "imagens", "grama_2.png"))
         self.grama_3 = pygame.image.load(os.path.join(self.current_dir, "imagens", "grama_3.png"))
         self.rua = pygame.image.load(os.path.join(self.current_dir, "imagens", "rua_full.png"))
-        self.rio = pygame.image.load(os.path.join(self.current_dir, "imagens", "rio_full.png"))
         self.linha_chegada = pygame.image.load(os.path.join(self.current_dir, "imagens", "chegada.png"))
 
     def desenhar(self, tela):
@@ -19,11 +19,13 @@ class Cenario:
         tela.blit(self.linha_chegada, (0, 0))
         # Grama 3
         tela.blit(self.grama_3, (0, self.linha_chegada.get_height()))
-        # Rio
-        tela.blit(self.rio, (0, self.linha_chegada.get_height() + self.grama_3.get_height()))
-        # Grama 2
-        tela.blit(self.grama_2, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rio.get_height()))
         # Rua
-        tela.blit(self.rua, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rio.get_height() + self.grama_2.get_height()))
+        tela.blit(self.rua, (0, self.linha_chegada.get_height() + self.grama_3.get_height()))
+        # Grama 2
+        tela.blit(self.grama_2, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rua.get_height()))
+        # Rua
+        tela.blit(self.rua, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rua.get_height() + self.grama_2.get_height()))
         # Grama 1
-        tela.blit(self.grama_1, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rio.get_height() + self.grama_2.get_height() + self.rua.get_height()))
+        tela.blit(self.grama_1, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rua.get_height() + self.grama_2.get_height() + self.rua.get_height()))
+        # Grama 0
+        tela.blit(self.grama_0, (0, self.linha_chegada.get_height() + self.grama_3.get_height() + self.rua.get_height() + self.grama_2.get_height() + self.rua.get_height() + self.grama_0.get_height()))

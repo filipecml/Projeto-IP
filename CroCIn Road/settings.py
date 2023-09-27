0c7297281974fcb56ca4d84487fdd9f48d642626
Filipe Moreira <fmc@cin.ufpe.br>
2023-09-26 21:45:09 -0300

    Update settings.py

--- CroCIn Road/settings.py
+++ CroCIn Road/settings.py
@@ -5,8 +5,8 @@ altura = 800
 # Configurações dos carros
 tempo_de_spawn_carro_azul = 1750
 tempo_de_spawn_carro_vermelho = 2000
-tempo_de_spawn_vans = 1500
-tempo_de_spawn_trucks = 2250
+tempo_de_spawn_vans = 2500
+tempo_de_spawn_trucks = 3000
 
 # Tamanho do personagem
 tamanho_personagem = 40

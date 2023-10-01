79846cb6c59cbcc69cbfcfa56098c7615ec212f6
Filipe Moreira <fmc@cin.ufpe.br>
2023-10-01 18:13:45 -0300

    Update menu.py

--- CroCIn Road/menu.py
+++ CroCIn Road/menu.py
@@ -5,70 +5,103 @@ import os
 class Menu:
     def __init__(self):
         self.current_dir = os.path.dirname(__file__)
+        self.instrucoes = False
         
         self.background = pygame.image.load(os.path.join(self.current_dir, "imagens", "fundo_menu.jpg"))
-        self.start_button = pygame.image.load(os.path.join(self.current_dir, "imagens", "botao_jogar.png"))
-        self.exit_button = pygame.image.load(os.path.join(self.current_dir, "imagens", "botao_sair.png"))
-    
+        self.start_button = pygame.transform.scale(pygame.image.load(os.path.join(self.current_dir, "imagens", "botao_jogar.png")), (199, 72))
+        self.exit_button = pygame.transform.scale(pygame.image.load(os.path.join(self.current_dir, "imagens", "botao_sair.png")), (199, 72))
+        self.instructions_button = pygame.transform.scale(pygame.image.load(os.path.join(self.current_dir, "imagens", "botao_instrucoes.png")), (199, 72))
+        self.instructions_text = pygame.image.load(os.path.join(self.current_dir, "imagens", "instrucoes.png"))
+
+        self.menu_music = os.path.join(self.current_dir, "sons", "menu_music.mp3")
+        self.click_sound = os.path.join(self.current_dir, "sons", "click_on_menu.wav")
+
+    def play_menu_music(self):
+        pygame.mixer.music.load(self.menu_music)
+        pygame.mixer.music.set_volume(0.25)
+        pygame.mixer.music.play(-1)
+
     def desenhar(self, tela):
         tela.blit(self.background, (0, 0))
-        tela.blit(self.start_button, (175, 320))
-        tela.blit(self.exit_button, (175, 465))
-    
+        tela.blit(self.start_button, (205, 300))
+        tela.blit(self.exit_button, (205, 400))
+        tela.blit(self.instructions_button, (205, 500))
+
     def verifica_clique(self, click_position):
-        start = False
-        sair = False
-        
-        # Verifica o botão de start
+        start, sair, instrucoes = False, False, False
+
         if (
-            click_position[0] <= 440 and click_position[0] >= 180
-            and click_position[1] <= 408 and click_position[1] >= 326
+            click_position[0] <= 405 and click_position[0] >= 205
+            and click_position[1] <= 365 and click_position[1] >= 300
         ):
             start = True
-        # Verifica o botão de exit
         elif (
-            click_position[0] <= 440 and click_position[0] >= 180
-            and click_position[1] <= 560 and click_position[1] >= 470
+            click_position[0] <= 405 and click_position[0] >= 205
+            and click_position[1] <= 465 and click_position[1] >= 405
         ):
             sair = True
-        
-        return start, sair
+        elif (
+            click_position[0] <= 405 and click_position[0] >= 205
+            and click_position[1] <= 565 and click_position[1] >= 505
+        ):
+            instrucoes = True
+
+        return start, sair, instrucoes
+
+    def mostra_instrucoes(self, tela):
+        tela.fill((0, 163, 108))
+        tela.blit(self.instructions_text, (100, 100))
+
+        fonte_instructions_escape = pygame.font.SysFont("bahnschrift", 25)
+        texto_instructions_escape = fonte_instructions_escape.render(">> Press 'ESC' to go back to menu <<", 1, (255, 255, 255))
+        tela.blit(texto_instructions_escape, (100, 675))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+
+        response = pygame.key.get_pressed()
+        if response[pygame.K_ESCAPE]:
+            self.instrucoes = False  # Voltando para o menu
+            self.play_menu_music()
 
     def executa_menu(self, tela):
-    
-        start, sair = False, False
-        
         os.chdir(self.current_dir)
 
-        pygame.mixer.music.load("sons\menu_music.mp3")
-        pygame.mixer.music.set_volume(0.25)
-        pygame.mixer.music.play()
-        
-        # Loop de execução do menu
+        self.play_menu_music()
         while True:
-            # Desenhando o menu na tela
             self.desenhar(tela)
-            
+
             for event in pygame.event.get():
-                # Verifica o fim da execução do programa
                 if event.type == QUIT:
                     pygame.quit()
                     exit()
-                # Verifica se houve um clique no botão do mouse e se o botão foi o esquerdo
                 elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
-                    # Verifica qual dos botões do menu foi selecionado
-                    start, sair = self.verifica_clique(pygame.mouse.get_pos())
-            
-            if start:
-                pygame.mixer.music.stop()
-                
-                pygame.mixer.music.load("sons\click_on_menu.wav")
-                pygame.mixer.music.play()
-                
-                break
-            elif sair:
-                pygame.quit()
-                exit()
+                    start, sair, instrucoes = self.verifica_clique(pygame.mouse.get_pos())
+
+                    if start:
+                        pygame.mixer.music.stop()
+                        pygame.mixer.music.load(self.click_sound)
+                        pygame.mixer.music.play()
+                        return
+                    elif instrucoes:
+                        pygame.mixer.music.stop()
+                        pygame.mixer.music.load(self.click_sound)
+                        pygame.mixer.music.play()
+                        self.instrucoes = True
+                    elif sair:
+                        pygame.mixer.music.stop()
+                        pygame.mixer.music.load(self.click_sound)
+                        pygame.mixer.music.play()
+                        
+                        while pygame.mixer.music.get_busy():
+                            pygame.time.Clock().tick(5)
+                            
+                        pygame.quit()
+                        exit()
+
+            if self.instrucoes:
+                self.mostra_instrucoes(tela)
             
             pygame.display.update()
-            
